72cca602dc007abb887a5ecbd49016c2314295bc
MuRibeiro <mucostaribeiro@gmail.com>
2023-02-17 17:40:35 -0300

    Conta Bancaria

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,20 +2,52 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		
 		Scanner leia = new Scanner(System.in);
 		
+		//Criamos o Objeto Conta
+		Conta c1 = new Conta(1, 123, 1, "Murilo Ribeiro", 30000.f);// instaciamos o objeto CONTA e entramos com os dados
+		
+		//Visualizamos os dados da conta c1
+		c1.visualizar(); // chama o metodo para mostrar todas as informações na tela
+		
+		//Alteramos o Saldo da Conta c1
+		c1.setSaldo(35000.0f); // chama o set para alterar a informação do saldo
+		
+		//Visualizamos apenas o Saldo da Conta c1
+		System.out.println("\n" + c1.getSaldo());// chama o get para mostrar o saldo alterado
+		
+		//Criamos a Conta c2
+		Conta c2 = new Conta(2, 123, 2, "Juliana Ribeiro", 300000.f);// instaciamos o objeto CONTA e entramos com os dados
+		
+		//Visualizamos a Conta c2
+		c2.visualizar();
+		
+		//Efetuamos um Saque na Conta c2 e visualizamos e novo Saldo 
+		if(c2.sacar(1000000.0f));
+			System.out.println("\nSaldo dísponivel: " + c2.getSaldo());
+		
+		//Efetuamos um Deposito na Conta c1
+		c1.depositar(10000.0f);
+		
+		//Visualizamos os dados da Conta c1 após o Depósito
+		c1.visualizar();
+		
+		
 		int opcao;
 		
 		while (true) {
 			
-			System.out.println("*****************************************************************");
-			System.out.println("                                                                 ");
-			System.out.println("                   BANCO DO BRAZIL COM Z                         ");
-			System.out.println("                                                                 ");
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND + "*****************************************************************");
+			System.out.println("*                                                               *");
+			System.out.println("*                   BANCO DO BRAZIL COM Z                       *");
+			System.out.println("*                                                               *");
 			System.out.println("*****************************************************************");
 			System.out.println("                                                                 ");
 			System.out.println("             1 - Criar Conta                                     ");
@@ -30,7 +62,7 @@ public class Menu {
 			System.out.println("                                                                 ");
 			System.out.println("*****************************************************************");
 			System.out.println("Informe a opção desejada:                                        ");
-			System.out.println("                                                                 ");
+			System.out.println("                                                                 " + Cores.TEXT_RESET);
 			
 			opcao = leia.nextInt();
 			
